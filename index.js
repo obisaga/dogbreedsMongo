c84c27849d0254246e856130d18e5d037ec9bb8a
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-10-14 16:20:56 +0200

    final

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express"
 import "dotenv/config"
 import client from "./db/db.js"
 import dogsRouter from "./routes/dogs.js"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/dogs", dogsRouter)
 
@@ -17,5 +19,8 @@ client.on("connected", () => {
     app.listen(port, () => {
         console.log(`Server listening on port ${port}`)
     })
+    app.listen(80, function () {
+        console.log('CORS-enabled web server listening on port 80')
+      })
 })
 
--- routes/dogs.js
+++ routes/dogs.js
@@ -3,59 +3,76 @@ import Dog from "../models/Dog.js";
 
 const dogsRouter = express.Router()
 
+
+//error handling
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const statusMessage = err.message || "Internal server error";
+    res.status(statusCode).json({error: statusMessage})
+}
+
+// dogsRouter.use(errorHandler)
+
+
 //create a new dog
-dogsRouter.post("/", async (req, res) => {
+dogsRouter.post("/", async (req, res, next) => {
     try {
         const {breedName, dogImg, dogAbout, sportNeed, affectionateLevel} = req.body;
         const response = await Dog.create({breedName, dogImg, dogAbout, sportNeed, affectionateLevel})
-        res.status(201).json(response)
+        res.status(201).json(response) 
+        
     } catch (error) {
-        res.status(500).json(error)
+        res.status(401).json({message: "Invalid entry"})
+        next()
     }
-})
+}, errorHandler)
 
 
 //get all dogs
-dogsRouter.get("/", async (req,res) => {
+dogsRouter.get("/", async (req, res, next) => {
     try {
         const response = await Dog.find()
-        res.status(201).json(response)
+        res.status(200).json(response)
     } catch (error) {
-        res.status(500).json(error)
+        next()
     }
-})
+}, errorHandler)
+
 
 //get a dog by breed name
-dogsRouter.get("/name/:breedName", async (req, res) => {
-    const {breedName} = req.params
-
+dogsRouter.get("/name/:breedName", async (req, res, next) => {
     try {
-      const response = await Dog.find({breedName: breedName});
-      res.json(response)    } 
-
-catch(err){
-        res.status(500).json(err)
+      const {breedName} = req.params
+      const response = await Dog.findOne({breedName: breedName});
+      
+      if (!response) {
+        res.status(404).json({message: `Dog with a name ${breedName} doesn't exist`}) //checks if the dog breed is the database
+    } else {
+        res.status(200).json(response)    
     }
-})
+      
+    } catch(err) {
+       next()
+    }
+}, errorHandler)
 
 
 //get a dog by id
 dogsRouter.get("/id/:id", async (req,res) => {
     try {
         const {id} = req.params
-        // console.log(breedName)
         const response = await Dog.findById({_id: id})
-        // console.log(response)
 
         if(!id){
-            return res.status(404).json({message: "Id not found"})
+            return res.status(404).json({message: `Dog with id ${id} doesn't exist`})
         }
 
         res.status(201).json(response)
     } catch (error) {
-        res.status(500).json(error)
+        next()
     }
-})
+}, errorHandler)
+
 
 //edit a dog
 dogsRouter.put("/:breedName", async (req, res) => {
@@ -63,7 +80,7 @@ dogsRouter.put("/:breedName", async (req, res) => {
         const {breedName} = req.params
         const {newName, newImg, newAbout, newSport, newAffection} = req.body
         const response = await Dog.findOneAndUpdate({breedName}, {$set: {breedName: newName, dogImg: newImg, dogAbout: newAbout, sportNeed: newSport, affectionateLevel: newAffection}}, {new: true})
-        res.status(201).json(response)
+        res.status(200).json(response)
     } catch (error) {
         res.status(500).json(error)
     }
@@ -73,7 +90,7 @@ dogsRouter.put("/:breedName", async (req, res) => {
 dogsRouter.delete("/:breedName", async (req, res) => {
     try {
         const {breedName} = req.params
-        const response = await Dog.deleteOne(breedName)
+        const response = await Dog.deleteOne({breedName : breedName})
 
         if(!response){
             res.status(404).json({message: "Dog not found"})
