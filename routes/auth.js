7f30895c5d736bf4dc09577be5ecb4c6aa729e28
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-10-25 22:04:09 +0200

    updates

--- routes/auth.js
+++ routes/auth.js
@@ -1,69 +1,66 @@
-import express from "express"
+import express from "express";
 import User from "../models/User.js";
-import bcrypt from 'bcrypt';
-import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const authRouter = express.Router();
 
 const secret = process.env.SECRET;
 
-
 const generateToken = (data) => {
-    return jwt.sign(data, secret, {expiresIn: '1800s'})
-  }
-
+  return jwt.sign(data, secret, { expiresIn: "1800s" });
+};
 
 authRouter.post("/dogbreeds/register", async (req, res) => {
-    try {
-      const { username, password, email } = req.body;
+  try {
+    const { username, password, email } = req.body;
 
-      //check if the user exists in the database
-    const userExists = await User.findOne({username})
-    if(userExists){
-    return res.status(400).json({ message: "User already exists" })
+    //check if the user exists in the database
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
     }
 
-      //  Hash the password before saving to DB
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const response = await User.create({
-        username,
-        password: hashedPassword,
-        email,
-      });
-      res.status(201).json(response);
-    } catch (error) {
-        res.status(401).json({message: "Invalid entry"})
-    }
+    //  Hash the password before saving to DB
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const response = await User.create({
+      username,
+      password: hashedPassword,
+      email,
+    });
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(401).json({ message: "Invalid entry" });
+  }
 });
 
-
 authRouter.post("/dogbreeds/login", async (req, res) => {
-    try {
-      console.log(req.body);
-      const { username, password } = req.body;
+  try {
+    console.log(req.body);
+    const { username, password } = req.body;
 
-      //check if the user exists in db
-      const user = await User.findOne({ username });
-      if (!user) {
-        return res.status(400).json({ message: "Invalid credentials" });
-      }
-
-      //Compare passwords
-      const validPassword = await bcrypt.compare(password, user.password);
-      if (!validPassword) {
-        return res.status(400).json({ message: "Invalid credentials" });
-      }
-
-      //generate token
-      const token = generateToken({ username: user.username });
-
-      res.set("token", token);
-      res.set("Access-Control-Expose-Headers", "token");
-
-      res.json({ token });
-    } catch (error) {
-        res.status(401).json({message: "Invalid credentials"})
+    //check if the user exists in db
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
+
+    //Compare passwords
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    //generate token
+    const token = generateToken({ username: user.username });
+
+    res.set("token", token);
+    res.set("Access-Control-Expose-Headers", "token");
+
+    res.json({ token });
+  } catch (error) {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
 });
 
 export default authRouter;
--- routes/dogs.js
+++ routes/dogs.js
@@ -1,127 +1,147 @@
 import express from "express";
 import Dog from "../models/Dog.js";
 import User from "../models/User.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const dogsRouter = express.Router()
+const dogsRouter = express.Router();
 const secret = process.env.SECRET;
 
 //error handling
 const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const statusMessage = err.message || "Internal server error";
-    res.status(statusCode).json({error: statusMessage})
-}
+  const statusCode = err.statusCode || 500;
+  const statusMessage = err.message || "Internal server error";
+  res.status(statusCode).json({ error: statusMessage });
+};
 
-  //authentication middleware
-  const auth = (req, res, next) => {
-    const token = req.headers.authorization
-    console.log(token)
-    
-    if(!token){
-        return res.sendStatus(401)
+//authentication middleware
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log(token);
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secret, (err, user) => {
+    if (err) {
+      return res.sendStatus(401);
     }
-    // const tokenData = token.split(' ')[1];
-
-    jwt.verify(token, secret, (err, user) => {
-        if(err){
-            return res.sendStatus(401)
-        }
-        req.user = user  
-        next()
-    })
-}
-
+    req.user = user;
+    next();
+  });
+};
 
 //create a new dog
 dogsRouter.post("/", async (req, res, next) => {
     try {
-        const {breedName, dogImg, dogAbout, sportNeed, affectionateLevel} = req.body;
-        const response = await Dog.create({breedName, dogImg, dogAbout, sportNeed, affectionateLevel})
-        res.status(201).json(response) 
-        
+      const { breedName, dogImg, dogAbout, sportNeed, affectionateLevel } =
+        req.body;
+      const response = await Dog.create({
+        breedName,
+        dogImg,
+        dogAbout,
+        sportNeed,
+        affectionateLevel,
+      });
+      res.status(201).json(response);
     } catch (error) {
-        res.status(401).json({message: "Invalid entry"})
-        next()
+      res.status(401).json({ message: "Invalid entry" });
+      next();
     }
-}, errorHandler)
-
-
+  },
+  errorHandler
+);
 
 //get all dogs
-dogsRouter.get("/", async (req, res, next) => {
+dogsRouter.get("/", auth, async (req, res, next) => {
     try {
-        const response = await Dog.find()
-        res.status(200).json(response)
-    } catch(err) {
-        res.status(500).json(err)
-        
+      const response = await Dog.find();
+      res.status(200).json(response);
+    } catch (err) {
+      res.status(500).json(err);
     }
-    next()
-}, errorHandler)
-
+    next();
+  },
+  errorHandler
+);
 
 //get a dog by breed name
 dogsRouter.get("/name/:breedName", async (req, res, next) => {
     try {
-      const {breedName} = req.params
-      const response = await Dog.findOne({breedName: breedName});
-      
-      if (!response) {
-        res.status(404).json({message: `Dog with a name ${breedName} doesn't exist`}) //checks if the dog breed is the database
-    } else {
-        res.status(200).json(response)    
-    }
-      
-    } catch(err) {
-       next()
-    }
-}, errorHandler)
+      const { breedName } = req.params;
+      const response = await Dog.findOne({ breedName: breedName });
 
+      if (!response) {
+        res
+          .status(404)
+          .json({ message: `Dog with a name ${breedName} doesn't exist` }); //checks if the dog breed is the database
+      } else {
+        res.status(200).json(response);
+      }
+    } catch (err) {
+      next();
+    }
+  },
+  errorHandler
+);
 
 //get a dog by id
-dogsRouter.get("/id/:id", async (req,res, next) => {
+dogsRouter.get("/id/:id", async (req, res, next) => {
     try {
-        const {id} = req.params
-        const response = await Dog.findById({_id: id})
+      const { id } = req.params;
+      const response = await Dog.findById({ _id: id });
 
-        if(!id){
-            return res.status(404).json({message: `Dog with id ${id} doesn't exist`})
-        }
+      if (!id) {
+        return res
+          .status(404)
+          .json({ message: `Dog with id ${id} doesn't exist` });
+      }
 
-        res.status(201).json(response)
+      res.status(201).json(response);
     } catch (error) {
-        next()
+      next();
     }
-}, errorHandler)
-
+  },
+  errorHandler
+);
 
 //edit a dog
 dogsRouter.put("/:breedName", async (req, res) => {
-    try {
-        const {breedName} = req.params
-        const {newName, newImg, newAbout, newSport, newAffection} = req.body
-        const response = await Dog.findOneAndUpdate({breedName}, {$set: {breedName: newName, dogImg: newImg, dogAbout: newAbout, sportNeed: newSport, affectionateLevel: newAffection}}, {new: true})
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+  try {
+    const { breedName } = req.params;
+    const { newName, newImg, newAbout, newSport, newAffection } = req.body;
+    const response = await Dog.findOneAndUpdate(
+      { breedName },
+      {
+        $set: {
+          breedName: newName,
+          dogImg: newImg,
+          dogAbout: newAbout,
+          sportNeed: newSport,
+          affectionateLevel: newAffection,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 //delete a dog
 dogsRouter.delete("/:breedName", async (req, res) => {
-    try {
-        const {breedName} = req.params
-        const response = await Dog.deleteOne({breedName : breedName})
+  try {
+    const { breedName } = req.params;
+    const response = await Dog.deleteOne({ breedName: breedName });
 
-        if(!response){
-            res.status(404).json({message: "Dog not found"})
-        }
-        res.status(201).json(response)
-    } catch (error) {
-        res.status(500).json(error)
+    if (!response) {
+      res.status(404).json({ message: "Dog not found" });
     }
-})
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-
-export default dogsRouter
+export default dogsRouter;
